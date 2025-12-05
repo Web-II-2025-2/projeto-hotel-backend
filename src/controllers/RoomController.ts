50ae51f91fa073c200514cce0f7877068d5b9dd1
Arthurabriel <arthurgb62@gmail.com>
2025-12-05 18:46:04 -0300

    feat: implement room creation and update validation with Zod schemas and error handling

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -7,47 +7,28 @@ const roomService = new RoomService();
 export class RoomController {
 
     async createRoom(req: Request<{}, {}, RoomCreationAttributes>, res: Response) {
-        try {
-            const room = await roomService.createRoom(req.body);
-            return res.status(201).json(room);
-        } catch (e: any) {
-            return res.status(400).json({ error: e.message });
-        }
+        const room = await roomService.createRoom(req.body);
+        return res.status(201).json(room);
     }
 
     async getRoom(req: Request, res: Response) {
-        try {
-            const room = await roomService.getRoom(Number(req.params.id));
-            return res.json(room);
-        } catch (e: any) {
-            return res.status(404).json({ error: e.message });
-        }
+        const room = await roomService.getRoom(Number(req.params.id));
+        return res.json(room);
     }
 
     async updateRoom(req: Request<{ id: string }, {}, RoomAttributes>, res: Response) {
-        try {
-            const room = await roomService.updateRoom(Number(req.params.id), req.body);
-            return res.json(room);
-        } catch (e: any) {
-            return res.status(404).json({ error: e.message });
-        }
+        const room = await roomService.updateRoom(Number(req.params.id), req.body);
+        return res.json(room);
     }
 
     async getAllRooms(req: Request, res: Response) {
-        try {
-            const rooms = await roomService.getAllRooms();
-            return res.json(rooms);
-        } catch (e: any) {
-            return res.status(400).json({ error: e.message });
-        }
+        const rooms = await roomService.getAllRooms();
+        return res.json(rooms);
+
     }
 
     async deleteRoom(req: Request, res: Response) {
-        try {
-            await roomService.deleteRoom(Number(req.params.id));
-            return res.status(204).send();
-        } catch (e: any) {
-            return res.status(404).json({ error: e.message });
-        }
+        await roomService.deleteRoom(Number(req.params.id));
+        return res.status(204).send();
     }
 }
--- src/services/RoomService.ts
+++ src/services/RoomService.ts
@@ -1,3 +1,4 @@
+import { AppError } from "../error/AppError";
 import Room, { RoomAttributes, RoomCreationAttributes } from "../models/Room";
 import { RoomRepository } from "../repository/RoomRepository";
 
@@ -5,18 +6,29 @@ export class RoomService {
   private roomRepository = new RoomRepository();
 
   async createRoom (data: RoomCreationAttributes): Promise<Room> {
+    const roomAlreadyExists = await this.roomRepository.findByNumber(data.number);
+
+    if (roomAlreadyExists) {
+      throw new AppError('Este número de quarto já está sendo utilizado.', 409);
+    }
     return await this.roomRepository.createRoom(data);
   }
 
   async getRoom(id: number): Promise<Room | null>  {
     const room = await this.roomRepository.getRoom(id);
-    if (!room) throw new Error("Room not found");
+    if (!room) throw new AppError('room not found.', 404);
     return room;
   }
   
   async updateRoom(id: number, data: RoomAttributes): Promise<Room | null> {
+    const roomAlreadyExists = await this.roomRepository.findByNumber(data.number);
+
+    if (roomAlreadyExists && roomAlreadyExists.id !== id) {
+      throw new AppError('Este número de quarto já está sendo utilizado.', 409);
+    }
+
     const room = await this.roomRepository.updateRoom(id, data);
-    if (!room) throw new Error("Room not found");
+    if (!room) throw new AppError("Room not found", 404);
     return room;
   }
 
@@ -26,7 +38,7 @@ export class RoomService {
 
   async deleteRoom(id: number) {
     const deleted = await this.roomRepository.deleteRoom(id);
-    if (!deleted) throw new Error("Room not found");
+    if (!deleted) throw new AppError("Room not found", 404);
     return true;
   }
 }
