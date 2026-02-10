96827c9a7db4854489988954aa7a5b7f0ece9d83
Arthurabriel <arthurgb62@gmail.com>
2026-02-10 19:43:15 -0300

    feat: refactor AuthController to implement guest and manager registration methods

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,25 +1,29 @@
 import { Request, Response } from "express";
 import { AuthService } from "../services/AuthService";
+import { RoleType } from "../enums/RoleType";
 
 const authService = new AuthService();
 
 export class AuthController {
 
-  register = async (req: Request, res: Response) => {
-    try {
-      const result = await authService.register(req.body);
-      return res.status(201).json(result);
-    } catch (err: any) {
-      return res.status(400).json({ message: err.message });
-    }
+
+  register_guest = async (req: Request, res: Response) => {
+    const result = await authService.registerGuest(req.body);
+    return res.status(201).json(result);
+  }
+  
+  register_manager = async (req: Request, res: Response) => {
+    const result = await authService.registerEmployee(req.body, RoleType.MANAGER);
+    return res.status(201).json(result);
   }
 
+  register_employee = async (req: Request, res: Response) => {
+    const result = await authService.registerEmployee(req.body, RoleType.EMPLOYEE);
+    return res.status(201).json(result);
+  }
+  
   login = async (req: Request, res: Response) => {
-    try {
-      const result = await authService.login(req.body);
-      return res.status(200).json(result);
-    } catch (err: any) {
-      return res.status(401).json({ message: err.message });
-    }
+    const result = await authService.login(req.body);
+    return res.status(200).json(result);
   }
 }
