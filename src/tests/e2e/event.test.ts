f893c8608d9aaf61cc249026aac2b7106ae5dbcf
Victor Vili <victor.vili.xavier.luna@ccc.ufcg.edu.br>
2026-02-13 22:11:46 -0300

    feat: add end-to-end test for event creation and user participation validation

---
+++ src/tests/e2e/event.test.ts
@@ -0,0 +1,72 @@
+import request from "supertest";
+import app from "../../app";
+import { createInitialAdmin } from "../../config/adminSeed";
+import { createAdminAndLogin, loginGuest, registerGuest } from "./helpers";
+
+describe(`CT-E2E-05: Criação de evento e usuário pede permissao para participar, quando segundo usuário
+     pedir para participar, o evento deve retornar cheio.`, () => {
+  let adminToken: string;
+  let registerRes1: any;
+  let registerRes2: any;
+  const user2Email = `user2_${Date.now()}@hotel.com`;
+  const user2Password = "user2Password123";
+  const user2Cpf = "22222222222";
+  const user2PhoneNumber = "222222222";
+
+  beforeAll(async () => {
+    await createInitialAdmin();
+    adminToken = await createAdminAndLogin(app);
+
+    registerRes1 = await registerGuest(app, {});
+    registerRes2 = await registerGuest(app, {
+      email: user2Email,
+      password: user2Password,
+      cpf: user2Cpf,
+      phoneNumber: user2PhoneNumber,
+    });
+  });
+
+  it(`Should create an event with capacity 1, then two different users should attempt to join,
+     the first should succeed and the second should fail`, async () => {
+        
+    const createEventRes = await request(app)
+      .post("/events")
+      .set("Authorization", `Bearer ${adminToken}`)
+      .send({
+        name: "Event Test",
+        description: "This is a test event",
+        date: new Date(),
+        time: "18:00",
+        capacity: 1,
+        location: "Hotel Lobby",
+      });
+
+    expect(createEventRes.status).toBe(201);
+
+    const eventId = createEventRes.body.id;
+
+    const user1Token = await loginGuest(
+      app,
+      registerRes1.data.email,
+      registerRes1.data.password,
+    );
+
+    const user2Token = await loginGuest(
+      app,
+      registerRes2.data.email,
+      registerRes2.data.password,
+    );
+
+    const joinEventRes1 = await request(app)
+      .post(`/events/${eventId}/join`)
+      .set("Authorization", `Bearer ${user1Token}`);
+
+    expect(joinEventRes1.status).toBe(200);
+    const joinEventRes2 = await request(app)
+      .post(`/events/${eventId}/join`)
+      .set("Authorization", `Bearer ${user2Token}`);
+
+    expect(joinEventRes2.status).toBe(400);
+
+    });
+});
