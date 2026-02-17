bc93f99a900c828ca2c1b9b3140025a079043082
Arthurabriel <arthurgb62@gmail.com>
2026-02-17 08:35:48 -0300

    feat: add error and log middleware for enhanced error handling and logging

--- src/middleware/errorMiddleware.ts
+++ src/middleware/errorMiddleware.ts
@@ -1,19 +1,26 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "../error/AppError";
+import logger from "../utils/logger";
 
 export const errorMiddleware = (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction) => {
+  const statusCode = err instanceof AppError ? err.statusCode : 500;
+  if (statusCode >= 500) {
+    logger.error(`${statusCode} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`, {
+      stack: err.stack 
+    });
+  } else {
+    logger.warn(`${statusCode} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+  }
   if (err instanceof AppError) {
     return res.status(err.statusCode).json({
       status: "error",
       message: err.message,
     });
   }
-
-  console.error(err);
 
   return res.status(500).json({
     status: "error",
