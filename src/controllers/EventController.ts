21d6e855a57735e8d03ae2791dc239e369c37774
Arthurabriel <arthurgb62@gmail.com>
2026-02-25 17:48:58 -0300

    refactor: simplify event controller methods by removing error handling

--- src/controllers/EventController.ts
+++ src/controllers/EventController.ts
@@ -1,53 +1,37 @@
 import { Request, Response } from "express";
 import { EventService } from "../services/EventService";
 import { EventAttributes, EventCreationAttributes } from "../models/Event";
+import { UserAuthRequest } from "../config/request";
+
 
 const eventService = new EventService();
 
 export class EventController {
 
     async createEvent(req: Request<{}, {}, EventCreationAttributes>, res: Response) {
-        try {
-            const event = await eventService.create(req.body);
-            return res.status(201).json(event);
-        } catch (e: any) {
-            return res.status(400).json({ error: e.message });
-        }
+        const event = await eventService.create(req.body);
+        return res.status(201).json(event);
     }
 
     async getEvent(req: Request, res: Response) {
-        try {
-            const event = await eventService.getById(Number(req.params.id));
-            return res.json(event);
-        } catch (e: any) {
-            return res.status(404).json({ error: e.message });
-        }
+        const event = await eventService.getById(Number(req.params.id));
+        return res.json(event);
     }
 
     async updateEvent(req: Request<{ id: string }, {}, EventAttributes>, res: Response) {
-        try {
-            const event = await eventService.update(Number(req.params.id), req.body);
-            return res.json(event);
-        } catch (e: any) {
-            return res.status(404).json({ error: e.message });
-        }
+        const event = await eventService.update(Number(req.params.id), req.body);
+        return res.json(event);
     }
 
     async getAllEvents(req: Request, res: Response) {
-        try {
-            const event = await eventService.getAll();
-            return res.json(event);
-        } catch (e: any) {
-            return res.status(400).json({ error: e.message });
-        }
+        const events = await eventService.getAll();
+        return res.json(events);
     }
 
     async deleteEvent(req: Request, res: Response) {
-        try {
-            await eventService.delete(Number(req.params.id));
-            return res.status(204).send();
-        } catch (e: any) {
-            return res.status(404).json({ error: e.message });
-        }
+        await eventService.delete(Number(req.params.id));
+        return res.status(204).send();
     }
+
+
 }
